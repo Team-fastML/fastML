9e42c75b480e3e85886abdf07c68575056b841c2
labaran1 <adlabaran@gmail.com>
2020-05-25 12:16:34 -0400

    Test changes to autoEncode Categorical Data

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from fastML import fastML
+from fastML import EncodeCategorical
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
+
 
 df = pd.read_csv('Iris.csv')
 X = df[['SepalLengthCm', 'SepalWidthCm',
         'PetalLengthCm', 'PetalWidthCm']].values
 Y = df['Species'].values
-le = preprocessing.LabelEncoder()
-Y = le.fit_transform(Y)
+
+Y = EncodeCategorical(Y)
+
 
 fastML(X, Y, size=.30)
