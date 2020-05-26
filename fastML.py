76885b05fbaa9cfc933cfced86d0eeb74ade5dc1
buabaj <buabajerry@gmail.com>
2020-05-26 14:57:21 +0000

    optimized code in fastML

--- fastML.py
+++ fastML.py
@@ -2,89 +2,51 @@
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+
 import pandas as pd
 
 ##creating a function to handle classification algorithms
 
-def fastML(X, Y, size): #defining arguments to be passed in function
-
-    ##assigning various classification algoritms to variables for easy accessibilty
-    svc = SVC()
-    KNN = KNeighborsClassifier()
-    DTC = DecisionTreeClassifier()
-    RF = RandomForestClassifier()
+def fastML(X, Y, size, *args): #defining arguments to be passed in function
 
     ##splitting the data into training and testing data and setting the test_size
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=size)
 
-    ##training the models with the train data
-    svc.fit(X_train, y_train)
-    KNN.fit(X_train, y_train)
-    DTC.fit(X_train, y_train)
-    RF.fit(X_train, y_train)
+    ##training the model with train data
+    acc_score = []
+    for alg in args:
+        alg.fit(X_train, y_train)
+        prediction = alg.predict(X_test)
 
-    ##making model predictions on the test data
-    DTC_prediction = DTC.predict(X_test)
-    KNN_prediction = KNN.predict(X_test)
-    SVC_prediction = svc.predict(X_test)
-    RF_prediction = RF.predict(X_test)
+        ##outputing statistics on the performance of the individual models
+        ##statistics printed out are the accuracy score, confusion matrix and clasification report
+        print('____________________________________________________')
+        print('____________________________________________________')
+        print("Accuracy Score for "+ alg.__class__.__name__+" is \n"+ str(accuracy_score(y_test, prediction)))
+        print('\n')
 
-    ##outputing statistics on the performance of all individual model
-    ##the statistics to be printed are the accuracy score, confusion matrix and classification report
-    print(
-        "-"*50 + "\nDesicion Tree Classifier\n" + "-"*50 +
-        "\nAccuracy score for Decision tree Classifier is " + str(accuracy_score(y_test, DTC_prediction)))
-    print(
-        "\nConfusion Matrix for Decision tree Classifier is \n" + str(confusion_matrix(y_test, DTC_prediction)))
-    print(
-        "Classification Report for Decision tree Classifier is \n" + str(classification_report(y_test, DTC_prediction)))
+        print("Confusion Matrix for "+ alg.__class__.__name__+" is \n"+ str(confusion_matrix(y_test, prediction)))
+        print('\n')
 
-    print(
-        "-"*50 + "\nK-Nearest Neighbors Classifier\n" + "-"*50 +
-        "\nAccuracy score for K-Nearest Neighbors is " + str(accuracy_score(y_test, KNN_prediction)))
-    print(
-        "\nConfusion Matrix for K-Nearest Neighbors is \n" + str(confusion_matrix(y_test, KNN_prediction)))
-    print(
-        "Classification Report for K-Nearest Neighbors is \n" + str(classification_report(y_test, KNN_prediction)))
+        print("Classification Report for "+ alg.__class__.__name__+" is \n"+ str(classification_report(y_test, prediction)))
+        print('\n')
 
-    print(
-        "-"*50 + "\nSupport Vector Machine\n" + "-"*50 +
-        "\nAccuracy score for Support Vector Machine is " + str(accuracy_score(y_test, SVC_prediction)))
-    print(
-        "\nConfusion Matrix for Support Vector Machine is \n" + str(confusion_matrix(y_test, SVC_prediction)))
-    print(
-        "Classification Report for Support Vector Machine is \n" + str(classification_report(y_test, SVC_prediction)))
+        print('____________________________________________________')
+        print('____________________________________________________')
 
-    print(
-        "-"*50 + "\nRandom Forest Classifier\n" + "-"*50 +
-        "\nAccuracy score for Random Forest Classifier is " + str(accuracy_score(y_test, RF_prediction)))
-    print(
-        "\nConfusion Matrix for Random Forest Classifier \n" + str(confusion_matrix(y_test, RF_prediction)))
-    print(
-        "Classification Report for Random Forest Classifier is \n" + str(classification_report(y_test, RF_prediction)))
+        ##saving the accuracy scores of the individual models as variables
+        accuracy = str(accuracy_score(y_test, prediction))
 
-    ##saving the accuracy scores of individual models as variables
-    DTC_accuracy = str(accuracy_score(y_test, DTC_prediction))
-    KNN_accuracy = str(accuracy_score(y_test, KNN_prediction))
-    SVC_accuracy = str(accuracy_score(y_test, SVC_prediction))
-    RF_accuracy = str(accuracy_score(y_test, RF_prediction))
-
-    ##making a list of individual models and their accuracy scores
-    acc_score = [['Decision tree', DTC_accuracy], ['K-Nearest Neighbors', KNN_accuracy], ['Support Vector Machine',
-                                                                                          SVC_accuracy], ['Random Forest', RF_accuracy]]
+        ##making a list of individual models and their accuracy scores
+        acc_score.append([alg.__class__.__name__, accuracy])
     
     ##creating a dataframe of every individual model and accuracy score 
     df = pd.DataFrame(acc_score, columns=['Model', 'Accuracy'])
-    ##outputing the created dataframe
-    print("-"*45 + "\nClassifier-Accuracy DataFrame\n" + "-"*45)
+    ##outputing the created dataframe 
     print(df)
 
 ##creating another function for handling the categorial encoding of data 
 def EncodeCategorical(Y): ##defining the argument the function takes
     from sklearn.preprocessing import LabelEncoder ##importing the needed library to handle data encoding
     le = LabelEncoder() ## assigning the encoder function to a variable
-    return le.fit_transform(Y) ##returning the transformed encoded data
+    return le.fit_transform(Y) ##returning the transformed encoded data 
