9b715b0dd177cab7cc4623945629102bda90994f
buabaj <buabajerry@gmail.com>
2020-05-30 01:35:37 +0000

    took out the csv data and loaded directly fron sklearn datasets

--- test.py
+++ test.py
@@ -9,20 +9,21 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from specialClassificationModel import neuralnet
+from sklearn import datasets
+
 
 
 
 ##reading the Iris dataset into the code
-df = pd.read_csv('Iris.csv')
+df =  datasets.load_iris()
 
 ##assigning the desired columns to X and Y  in preparation for running fastML
-X = df[['SepalLengthCm', 'SepalWidthCm',
-        'PetalLengthCm', 'PetalWidthCm']].values
-Y = df['Species'].values
+X = df.data[:, :4]
+Y = df.target
 
 ##running the EncodeCategorical function from fastML to handle the process of categorial encoding of data
 Y = EncodeCategorical(Y)
-size = 0.35
+size = 0.33
 ## running the fastML function from fastML to run multiple classification algorithms on the given data
 fastML(X, Y, size, SVC(), RandomForestClassifier(), DecisionTreeClassifier(), KNeighborsClassifier(), LogisticRegression(max_iter = 7000), special_classifier_epochs=200,special_classifier_nature ='fixed',
           include_special_classifier = True,)
