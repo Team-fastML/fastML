8111c7492887d62a3c964a72b9af2dbdb8532e59
buabaj <buabajerry@gmail.com>
2020-06-04 17:49:42 +0000

    testing beta release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: AI / ML Engineers',      # Define that your audience are developers
+    'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Machine Learning',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
