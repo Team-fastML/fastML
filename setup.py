4acbc8fe53154c20ca8b9b5ce4df61eba424086f
buabaj <buabajerry@gmail.com>
2020-06-04 18:03:25 +0000

    fastML i an occupied name so i changed package name to fast-ML

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 from distutils.core import setup
 setup(
-  name = 'fastML',         # How you named your package folder (MyLib)
-  packages = ['fastML'],   # Chose the same as "name"
+  name = 'fast-ML',         # How you named your package folder (MyLib)
+  packages = ['fast-ML'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Python package built with sklearn for running multiple classification algorithms in as little as 4 lines. This package drastically makes the work of Data Scientists, AI and ML engineers very easy and fast by saving them the physical stress of writing close to 200 lines of code as they would if not for this package.',   # Give a short description about your library
