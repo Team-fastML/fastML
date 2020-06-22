5f18c0e3bf89010527502e008c28b22399da2c2c
buabaj <buabajerry@gmail.com>
2020-06-22 20:42:12 +0000

    published

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 from distutils.core import setup
 setup(
   name = 'fastML',         # How you named your package folder (MyLib)
   packages = ['fastML'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Python package built with sklearn for running multiple classification algorithms to observe their behaviour in as little as 4 lines. This package drastically makes the work of Data Scientists, AI and ML engineers very easy and fast by saving them the physical stress of writing close to 300 lines of code as they would if not for this package.',   # Give a short description about your library
   author = 'Jerry Buaba',                   # Type in your name
@@ -25,6 +31,8 @@ setup(
           'pytz',
           'threadpoolctl'
       ],
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
